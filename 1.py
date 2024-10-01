1960a1370aed3ee464e0de95fd2e079d3991858e
udaykrishna289 <udaykrishna289@gmail.com>
2024-10-01 11:42:14 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+a=input("enter the number")
+b=input("enter the number")
+c=input("enter the number")
+if a<b:
+    print("b is greater than a")
+if b<c:
+    print("c is greater than b")
+if c<a:
+    print("c is greater than a")
+else:
+    print("enter the correct  data")
+
---
+++ 2.c
@@ -0,0 +1,6 @@
+a=10
+b=2.5
+c="uday"
+print(type(a))
+print(type(b))
+print(type(c))
